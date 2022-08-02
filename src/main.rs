20de7c76e2ef80221c53437c13681c46d93cc165
Dion Maicon E. Duarte <dionmaicon@outlook.com>
2022-08-02 18:20:56 +0100

    feat: add thread

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
+use std::collections::HashMap;
+use std::fmt::Debug;
 use std::process;
+use std::thread;
 
 use processing_bank_transactions_with_rust::{ read_csv, summarize_transactions, write_to_stdout };
+
+
+fn split<T>(arr: &[T], n: usize ) -> Vec<&[T]> where T: Debug {
+    let chunks: Vec<&[T]> = arr.chunks(n).collect();
+    chunks
+}
 
 fn main() {
     env_logger::init();
 
-    let mut transactions_by_client = match read_csv() {
+    let transactions_by_client = match read_csv() {
         Err(err) => {
             println!("Parsing process error: {}", err);
             process::exit(1);
@@ -13,13 +22,41 @@ fn main() {
         Ok(values) => values
     };
     
-    let accounts = match summarize_transactions(&mut transactions_by_client) {
-        Ok(values) => values,
-        Err(err) => {
-            println!("Summarize process error: {}", err);
-            process::exit(1);
+    let list: Vec<_> = transactions_by_client.into_iter().collect();
+    
+
+    let chunk_len = (list.len() / 4) as usize + 1;
+    let chunks: Vec<HashMap<_, _>> = list.chunks(chunk_len)
+        .map(|c| c.iter().cloned().collect())
+        .collect();
+
+    let subsets_chunks = split(&chunks, 2);
+           
+    let mut summarize_threads = Vec::new();
+    for set_chunk in subsets_chunks {
+        let cloned_sets =  set_chunk.to_vec();
+        for set in cloned_sets {
+           let th = thread::spawn( move || {
+           return match summarize_transactions(&mut set.clone()) {
+                Ok(values) => values,
+                Err(err) => {
+                    println!("Summarize process error: {}", err);
+                    process::exit(1);
+                }
+            };
+        });
+        summarize_threads.push(th);
         }
-    };
+    }
+
+    let mut accounts = Vec::new();
+    for sub in summarize_threads {
+        let result = sub.join().expect("Error");
+        let accs = result.to_vec();
+        for acc in accs {
+            accounts.push(acc);
+        }
+    }
 
     let _ = write_to_stdout(&accounts);
 }
